7de2582dfd7f0721b7ba8d5311c14f1897df369b
Guillermo Pineda <guillermopineda@Mac-mini-de-Guillermo.local>
2023-05-14 18:03:31 -0600

    debug

--- justsale_project/settings.py
+++ justsale_project/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'justsale_project.urls'
